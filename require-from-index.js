756b2d74c5d00f59800f407df3c075018a341556
yuey9507 <YLi@esri.com>
2018-07-13 14:07:41 +0800

    remove files and start to try to write plugin

--- require-from-index.js
+++
@@ -1,8 +0,0 @@
-window.replaceDynamicSrc = function (src) {
-	console.log("Manipulating ", src);
-	return "https://code.jquery.com/jquery-3.3.1.min.js";
-}
-
-window.setSDN = function () {
-	return "https://code.jquery.com/"
-}
